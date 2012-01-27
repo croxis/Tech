552c7fe8b5f35ff7a96c794bfb8e927e173c752d
croxis <croxis@yahoo.com>
2012-01-27 08:57:23 -0800

    Removed unused imports

--- src/main/java/me/croxis/plugins/tech/Tech.java
+++ src/main/java/me/croxis/plugins/tech/Tech.java
@@ -12,8 +12,6 @@ import org.bukkit.World;
 import org.bukkit.command.Command;
 import org.bukkit.command.CommandSender;
 import org.bukkit.entity.Player;
-import org.bukkit.event.Event.Priority;
-import org.bukkit.event.Event.Type;
 import org.bukkit.plugin.java.JavaPlugin;
 
 public class Tech extends JavaPlugin {
